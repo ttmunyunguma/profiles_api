43f31980d91ed69d803a804cc3a33d87b4aac960
Terrence <terrence@rubieminnovations.xyz>
2019-10-19 11:41:15 +0200

    Views and filters

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('', include(router.urls))
 ]
 
